c1d4a462172964145c99b11fedf605d79c47df78
Simon Thomas <sdat2@cam.ac.uk>
2022-03-28 10:27:08 +0100

    ext: create notebook

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="src",
     version="0.0.1",
     author="sdat2",
-    author_email="author@example.com",
+    author_email="sdat2@cam.ac.uk",
     description="Part II Computer Science Course Code",
-    url="url-to-github-page",
+    url="https://github.com/sdat2/MLBayesInfer",
     packages=find_packages(),
     test_suite="src.tests.test_all.suite",
 )
